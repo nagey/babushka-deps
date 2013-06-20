469fd38e528b77257dae371ffe7e1804a7543aa4
Stefan Nagey <stefan@nagey.com>
2013-06-20 17:38:50 -0400

    nodejs dep

---
+++ nodejs.rb
@@ -0,0 +1,16 @@
+dep 'nodejs.src', :version do
+  nodepath = "/usr/local/nodejs"
+  
+  requires "node_path".with(:nodepath => nodepath)
+  version.default!('0.10.11')
+  source "http://nodejs.org/dist/v#{version}/node-v#{version}.tar.gz"
+  provides "node ~> #{version}"
+  prefix nodepath
+  met? { in_path? "node >= #{version}" }
+end
+
+dep "node_path", :nodepath do
+  met? { shell? "echo $PATH | grep #{nodepath}" }
+  meet { sudo "echo PATH=#{nodepath}/bin:$PATH >> /etc/profile" }
+end
+  
